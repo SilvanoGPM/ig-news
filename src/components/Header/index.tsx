565017bc60811c64f7b6adece987e08c2cac5ff4
SkyG0D <silvanosilvino@hotmail.com>
2022-06-05 08:13:16 -0300

    feat: add SignInButton component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
-import styles from './styles.module.scss';
+import Image from "next/image";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
@@ -11,6 +12,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
